ca8c7e2c691cf0e3d25e52802fc94b3460b410bd
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-03-13 15:43:30 +0530

    init: initialize spring boot application

---
+++ employee-space-service/src/main/java/in/harmanpreetsingh/employeespace/EmployeespaceApplication.java
@@ -0,0 +1,13 @@
+package in.harmanpreetsingh.employeespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeespaceApplication.class, args);
+	}
+
+}
---
+++ employee-space-service/src/test/java/in/harmanpreetsingh/employeespace/EmployeespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.harmanpreetsingh.employeespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
